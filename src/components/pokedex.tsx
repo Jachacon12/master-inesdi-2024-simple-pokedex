5f424c216c9f535dd62dcff6196302b7c7ad7d35
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-09-23 10:54:09 +0200

    feat: Panels refactored to accommodate new design

--- src/components/pokedex.tsx
+++ src/components/pokedex.tsx
@@ -1,12 +1,11 @@
-import c from "classnames";
-import { useTheme } from "contexts/use-theme";
-import { usePokemon, usePokemonList, useTextTransition } from "hooks";
-import { useState } from "react";
-import { randomMode } from "utils/random";
-import { Button } from "./button";
-import { LedDisplay } from "./led-display";
-
-import "./pokedex.css";
+import c from 'classnames';
+import { useTheme } from 'contexts/use-theme';
+import { usePokemon, usePokemonList, useTextTransition } from 'hooks';
+import { useState } from 'react';
+import { randomMode } from 'utils/random';
+import { Button } from './button';
+import { LedDisplay } from './led-display';
+import './pokedex.css';
 
 export function Pokedex() {
   const { theme } = useTheme();
@@ -14,14 +13,13 @@ export function Pokedex() {
   const { pokemonList } = usePokemonList();
   const [i, setI] = useState(0);
   const { pokemon: selectedPokemon } = usePokemon(pokemonList[i]);
-  const { pokemon: nextPokemon } = usePokemon(pokemonList[i + 1]);
 
   const prev = () => {
     resetTransition();
     if (i === 0) {
       setI(pokemonList.length - 1);
     }
-    setI((i) => i - 1);
+    setI(i => i - 1);
   };
 
   const next = () => {
@@ -29,19 +27,42 @@ export function Pokedex() {
     if (i === pokemonList.length - 1) {
       setI(0);
     }
-    setI((i) => i + 1);
+    setI(i => i + 1);
   };
 
   return (
-    <div className={c("pokedex", `pokedex-${theme}`)}>
+    <div className={c('pokedex', `pokedex-${theme}`)}>
       <div className="panel left-panel">
+        <div className="left-top-container">
+          <svg
+            viewBox="0 0 300 100"
+            className="left-svg"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+          >
+            <polyline points="0,75 70,75 90,38 300,38" />
+          </svg>
+
+          <div className="lights-container">
+            <div className="big-light-boarder">
+              <div className="big-light blue">
+                <div className="big-dot light-blue"></div>
+              </div>
+            </div>
+            <div className="small-lights-container">
+              <LedDisplay color="red" />
+              <LedDisplay color="yellow" />
+              <LedDisplay color="green" />
+            </div>
+          </div>
+        </div>
         <div className="screen main-screen">
           {selectedPokemon && (
             <img
               className={c(
-                "sprite",
-                "obfuscated",
-                ready && "ready",
+                'sprite',
+                'obfuscated',
+                ready && 'ready',
                 ready && `ready--${randomMode()}`
               )}
               src={selectedPokemon.sprites.front_default}
@@ -52,39 +73,52 @@ export function Pokedex() {
         <div className="screen name-display">
           <div
             className={c(
-              "name",
-              "obfuscated",
-              ready && "ready",
+              'name',
+              'obfuscated',
+              ready && 'ready',
               ready && `ready--${randomMode()}`
             )}
           >
             {selectedPokemon?.name}
           </div>
         </div>
-      </div>
-      <div className="panel right-panel">
-        <div className="controls leds">
-          <LedDisplay color="blue" />
-          <LedDisplay color="red" />
-          <LedDisplay color="yellow" />
-        </div>
-        <div className="screen second-screen">
-          {nextPokemon && (
-            <img
-              className={c(
-                "sprite",
-                "obfuscated",
-                ready && "ready",
-                ready && `ready--${randomMode()}`
-              )}
-              src={nextPokemon.sprites.front_default}
-              alt={nextPokemon.name}
-            />
-          )}
-        </div>
         <div className="controls">
           <Button label="prev" onClick={prev} />
           <Button label="next" onClick={next} />
+        </div>
+      </div>
+      <div className="right-panel">
+        <div className="empty-container">
+          <svg
+            viewBox="0 0 270 100"
+            className="left-svg"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+          >
+            <polyline points="0,40 138,40 158,75 270,75"></polyline>
+          </svg>
+        </div>
+        <div className="screen second-screen">
+          {selectedPokemon && (
+            <div className="name-display information-display">
+              <details className="information-button">
+                <summary>Types</summary>
+                <div className="information-child">
+                  {selectedPokemon.types.slice(0, 3).map(a => (
+                    <p key={a.type?.name}>{a.type?.name}</p>
+                  ))}
+                </div>
+              </details>
+              <details className="information-button">
+                <summary>Weaknesses</summary>
+                <div className="information-child">
+                  {selectedPokemon.weaknesses.slice(0, 3).map(a => (
+                    <p key={a.weakness?.name}>{a.weakness?.name}</p>
+                  ))}
+                </div>
+              </details>
+            </div>
+          )}
         </div>
       </div>
     </div>
