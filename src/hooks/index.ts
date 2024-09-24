81aa22907fe8e76c7842a213164c0ce0142fa536
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-09-24 08:37:33 +0200

    feat: function to store selected pokemon in local storage

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,5 @@
-export { usePokemon } from "./use-pokemon";
-export { usePokemonList } from "./use-pokemon-list";
-export { useTextTransition } from "./use-text-transition";
+export { usePokemon } from './use-pokemon';
+export { usePokemonList } from './use-pokemon-list';
+export { useTextTransition } from './use-text-transition';
+export { addToTeam } from './addToTeam';
+export { showToaster } from './toast';
